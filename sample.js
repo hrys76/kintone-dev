81c4db7f9e7cbd7dfbf878b37679a93be03ceeac
hrys76 <wakino.hiroyoshi@gmail.com>
2021-08-27 13:35:21 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,1 +1,13 @@
-alert('Test');
+<!-- Begin TimeRex Widget --> 
+<div id="timerex_calendar" data-url="https://timerex.net/s/xxxxxxxxxx/xxxxxxxx"></div>
+<script id="timerex_embed" src="https://timerex.net/js/embed.js"></script>
+<script type="text/javascript">
+    TimerexCalendar({
+        'guest_company' : 'Mixtend Inc.',
+        'guest_name' : 'Tomohiro Kitano',
+        'guest_email' : 'xxxxxxxx@test.com',
+        'locale': 'ja',
+        'primary_color': '#2980b9'
+    });
+</script>
+<!-- End TimeRex Widget -->
