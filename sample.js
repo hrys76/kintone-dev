3ebb2df90f634e52e696cfbb1f41fa1a55adb405
hrys76 <wakino.hiroyoshi@gmail.com>
2021-08-28 22:05:55 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,14 +1,21 @@
 (function() {
   "use strict";
 
-  fb.events.form.created = [function (state) {
-    console.log(state);
-    console.log('test');
-//    state.form.title = 'edit form title';
-//    state.fields[0].hide = true;
-//    state.record.string1.value = 'edit string data';
+  fb.events.form.mounted = [function (state) {
+    // フォームのコンテンツ部分の要素を取得します
+    var formContent = document.getElementsByClassName('fb-content')[0];
 
-//    return state;  // 変更後の状態を返さない場合、一部の変更が反映されない場合があります
+    // ボタンを作成します
+    var submitButton = document.createElement('input');
+    submitButton.type = 'button';
+    submitButton.value = 'submit';
+    // ボタンが押された際に現在のフォームを強制的に回答するように設定します
+    submitButton.onclick = fb.forceSubmitForm;
+
+    // 作成したボタンをフォームのコンテンツ部分の要素の先頭に追加します
+    formContent.insertBefore(submitButton, formContent.firstChild);
+
+    return state;
   }];
 })();
 
