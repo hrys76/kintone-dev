d18ccb810de3c00a8d815c2fb8f172b862457fcf
hrys76 <wakino.hiroyoshi@gmail.com>
2021-08-28 20:37:05 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,7 +1,11 @@
-    TimerexCalendar({
+console.log(window);
+
+/*
+TimerexCalendar({
         'guest_company' : 'Mixtend Inc.',
         'guest_name' : 'Tomohiro Kitano',
         'guest_email' : 'xxxxxxxx@test.com',
         'locale': 'ja',
         'primary_color': '#2980b9'
     });
+*/
