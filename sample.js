72e08b47be69edfe2a134f2ab1387bfe6f6b624d
hrys76 <wakino.hiroyoshi@gmail.com>
2021-08-31 22:27:22 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,3 +1,10 @@
+<DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="https://form.kintoneapp.com/static/css/outside-form.css" rel="stylesheet" type="text/css" charset="UTF-8">
+    <script>
 (function() {
   "use strict";
 
@@ -19,4 +26,23 @@ TimerexCalendar({
         'primary_color': '#2980b9'
     });
 */
+    </script>
+      
+      </head>
+  <body>
+    <div id="app"></div>
+    <div id="based-data" locale="ja" host="https://form.kintoneapp.com" code="1f46a3c1fefc45994a4dfc89455f8ffa8dc73e513132107bd7e72b77f035b805"></div>
+    <script src="https://form.kintoneapp.com/static/js/layout-asset.js" type="text/javascript" charset="UTF-8"></script>
+    <script src="https://form.kintoneapp.com/static/js/outside-form.js" type="text/javascript" charset="UTF-8"></script>
 
+    <!-- Begin TimeRex Widget -->
+<!--
+    <div id="timerex_calendar" data-url="https://timerex.net/s/wakino.hiroyoshi/88767498"></div>
+<script id="timerex_embed" src="https://timerex.net/js/embed.js"></script>
+<script type="text/javascript">
+  TimerexCalendar();
+</script>
+-->
+<!-- End TimeRex Widget -->
+  </body>
+</html>
